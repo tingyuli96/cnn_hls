39803360456b6140a76a24660f0323cff4f35079
charlotte <904985646@qq.com>
2018-03-17 21:22:41 +0800

    v1.0

---
+++ feedforward.cpp
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WEIGHTSIZE 51902
+#define INPUTSIZE 1024
+#define OUTPUTSIZE 10
+
+#include "convolution.h"
+
+void CNN_Accel (AXI_VAL INPUT_STREAM[WEIGHTSIZE], AXI_VAL OUTPUT_STREAM[OUTPUTSIZE])
+{
+#pragma HLS INTERFACE ap_ctrl_none port=return
+	//#pragma HLS INTERFACE ap_ctrl_none port=return
+	//#pragma HLS INTERFACE s_axilite port=return     bundle=CONTROL_BUS
+	#pragma HLS INTERFACE axis      port=OUTPUT_STREAM
+	#pragma HLS INTERFACE axis      port=INPUT_STREAM
+
+	if(!weightsValid)
+	{
+		loadweights(INPUT_STREAM, OUTPUT_STREAM);
+		return;
+	}
+	else
+	{
+		accelerator(INPUT_STREAM, OUTPUT_STREAM);
+		return;
+	}
+
+
+}
+
